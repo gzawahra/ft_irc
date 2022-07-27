9c7321b1c09c8356865cce41a788baa59efed435
Gibran ZAWAHRA <gizawahr@e1r11p13.clusters.42paris.fr>
2022-07-27 15:45:32 +0200

    fix comment

--- test_val.sh
+++ test_val.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 make re
-if [ $USER = "gz" ] ; then
-terminator -e "valgrind --leak-check=yes --read-var-info=yes --track-origins=yes ./ircserv 6667 hello"
-else
-xterm -e "valgrind --leak-check=yes --read-var-info=yes --track-origins=yes ./ircserv 6667 hello"
-fi
-sleep 10s
+#if [ $USER = "gz" ] ; then
+terminator -e "valgrind --leak-check=yes --track-origins=yes ./ircserv 6667 hello"
+#else
+#xterm -e "valgrind --leak-check=yes --track-origins=yes ./ircserv 6667 hello"
+#fi
+sleep 3s
 nc 127.0.0.1 6667 < ./files/test_cmds
 echo "Press any key to continue"
 while [ true ] ; do
